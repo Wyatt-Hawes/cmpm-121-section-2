1e33a6c7c594adb95d48035fd7f3c528092782b6
Wyatt <jhawes@ucsc.edu>
2023-10-09 18:18:58 -0700

    done with partner 2

--- src/main.ts
+++ src/main.ts
@@ -12,18 +12,20 @@ let isJumping: boolean = false;
 let gameOver: boolean = true;
 
 SetText("click to start!");
-document.addEventListener("click", () => jump());
+document.addEventListener("mousedown", () => jump());
+document.addEventListener("keydown", () => jump());
 
 setInterval(() => {
-  Main();
+  update_score();
 }, 10);
 
-function Main() {
-  if (gameOver == false) {
-    score = score + 1;
-    SetText("Score: " + score);
-    CheckGameOver();
+function update_score() {
+  if (gameOver == true) {
+    return;
   }
+  score = score + 1;
+  SetText("Score: " + score);
+  CheckGameOver();
 }
 
 function jump() {
@@ -55,35 +57,33 @@ function get_position_property(obj: HTMLElement | null, property_name: string) {
 }
 
 function CheckGameOver() {
-    if (gameOver == true){
-        return;
-    }
-    //get is dinosaur jumping
-    let dinoTop = get_position_property(dino, "top") as number;
+  if (gameOver == true) {
+    return;
+  }
+  //get is dinosaur jumping
+  let dinoTop = get_position_property(dino, "top") as number;
 
-    //get cactus position
-    let cactusleft = get_position_property(cactus, "left") as number;
+  //get cactus position
+  let cactusleft = get_position_property(cactus, "left") as number;
 
-    //get bird position
-    let birdleft = get_position_property(bird, "left") as number;
+  //get bird position
+  let birdleft = get_position_property(bird, "left") as number;
 
-    //detect cactus collision
-    if (dinoTop >= 150 && Math.abs(cactusleft) < 7) {
-      //end game
-      end_game();
-    }
+  //detect cactus collision
+  if (dinoTop >= 150 && Math.abs(cactusleft) < 7) {
+    //end game
+    end_game();
+  }
 
-    //detect bird collision
-    if (dinoTop <= 55 && Math.abs(birdleft) < 11) {
-      //end game
-      end_game();
-    }
-  
+  //detect bird collision
+  if (dinoTop <= 55 && Math.abs(birdleft) < 11) {
+    //end game
+    end_game();
+  }
 }
 
 function end_game() {
   //end game
-  console.log("player died!");
   SetText("Final Score: " + score + "! Click To Play Again!");
   gameOver = true;
 
@@ -95,7 +95,6 @@ function end_game() {
 }
 
 function StartGame() {
-  console.log("Game started!");
   gameOver = false;
   score = 0;
   cactus?.classList.add("cactusMove");
